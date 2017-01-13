5662fe65eab4c20a4d99fb032a6cf7a7c8e266e1
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-01-13 16:29:56 +0100

    Adding a model classes.

---
+++ ZrrUniversity/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZrrUniversity.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollment { get; set; }
+    }
+}
---
+++ ZrrUniversity/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZrrUniversity.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollment { get; set; }
+    }
+}
