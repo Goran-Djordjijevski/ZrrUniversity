a730ba528b7ec7bbaa82c25dc38a36d056e3f1b8
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-01-15 13:51:59 +0100

    Add DbContext class and DbSet properties.

---
+++ ZrrUniversity/DataAccessLayer/UniversityContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ZrrUniversity.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ZrrUniversity.DataAccessLayer
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() :base("UniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
