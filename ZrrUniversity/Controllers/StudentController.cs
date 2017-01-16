bf0f2db959deecffdb9bab6d703a8cbc54f61bb9
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-01-16 09:39:39 +0100

    Update StudentController.

--- ZrrUniversity/Controllers/StudentController.cs
+++ ZrrUniversity/Controllers/StudentController.cs
@@ -47,13 +47,20 @@ namespace ZrrUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EnrollmentDate")] Student student)
+        public ActionResult Create([Bind(Include = "FirstName,LastName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
             }
 
             return View(student);
